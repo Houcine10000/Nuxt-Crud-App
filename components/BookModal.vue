2a7f45b9e6f5eacd5ce7c2252f4bf0d6ce3ca5ba
Houcine <swidyhoussine@gmail.com>
2024-07-29 14:50:23 +0100

    initial commit🚀

---
+++ components/BookModal.vue
@@ -0,0 +1,169 @@
+<template>
+  <TransitionRoot :show="open">
+    <!-- Dialog -->
+    <Dialog as="div" class="relative z-20" @close="closeModal">
+      <!-- Dialog overlay -->
+      <TransitionChild
+        enter="duration-200"
+        leave="duration-200"
+        enter-from="opacity-0"
+        leave-to="opacity-0"
+      >
+        <div class="fixed inset-0 bg-black/30 backdrop-blur"></div>
+      </TransitionChild>
+
+      <!-- Full-screen scrollable container -->
+      <div class="fixed inset-0 overflow-y-auto">
+        <!-- container to center the panel -->
+        <div class="flex items-center justify-center min-h-full p-4">
+          <!-- the actual dialog panel -->
+          <TransitionChild
+            enter="duration-300 ease-out"
+            enter-from="opacity-0 scale-95"
+            enter-to="opacity-100 scale-100"
+            leave="duration-300 ease-in"
+            leave-from="opacity-100 scale-100"
+            leave-to="opacity-0 scale-95"
+          >
+            <DialogPanel
+              class="w-full min-w-[360px] p-5 bg-white shadow-lg rounded-2xl"
+            >
+              <div class="flex items-center justify-between">
+                <!-- Conditional rendering for text -->
+                <DialogTitle class="text-xl font-medium text-gray-700">
+                  {{ book._id ? "update" : "create" }} Author
+                </DialogTitle>
+              </div>
+
+              <p class="mt-1 text-sm text-gray-500">
+                {{
+                  book._id ? "Update this author" : "Create a new author here"
+                }}
+              </p>
+
+              <form @submit="submitBook" class="mt-5">
+                <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+                  <div class="col-span-full">
+                    <FormInput
+                      v-model="author.name"
+                      rules="required"
+                      label="Title"
+                      type="text"
+                      name="title"
+                      id="title"
+                    />
+                  </div>
+
+                  <div class="mb-4 col-span-full">
+                    <FormAuthorInput v-model="author" />
+                  </div>
+
+                  <div class="col-span-1">
+                    <FormInput
+                      aria-placeholder="Select a date"
+                      rules="required"
+                      label="Published date"
+                      type="date"
+                      name="published"
+                      id="pub_date"
+                    />
+                  </div>
+
+                  <div class="col-span-1">
+                    <FormInput label="ISBN" type="text" name="isbn" id="isbn" />
+                  </div>
+
+                  <div class="col-span-1">
+                    <FormInput
+                      min="0"
+                      rules="required|min_value:0"
+                      label="Page count"
+                      type="number"
+                      name="pageCount"
+                      id="count"
+                    />
+                  </div>
+                </div>
+                <!-- Form buttons -->
+                <div class="flex items-center justify-end space-x-3 mt-7">
+                  <button
+                    @click="closeModal()"
+                    type="button"
+                    class="px-4 text-sm bg-gray-100 py-2.5 rounded-md"
+                  >
+                    Cancel
+                  </button>
+
+                  <button type="submit" class="btn">
+                    {{ book._id ? "Update" : "Create" }}
+                  </button>
+                </div>
+              </form>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup>
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionRoot,
+  TransitionChild,
+} from "@headlessui/vue";
+import { useForm } from "vee-validate";
+
+const authorStore = useAuthorStore();
+useAsyncData(() => authorStore.getAll(), { initialCache: false });
+
+// instantiate book store
+const bookStroe = useBookStore();
+
+const book = ref({});
+const authors = ref([]);
+
+// Get function used to handle from submission and set init form values
+const { handleSubmit } = useForm({
+  initialValues: book.value,
+});
+
+// func used to update or create the record
+const submitBook = handleSubmit(async (values, ctx) => {
+  if (!book.value._id) {
+    // create author
+    await bookStroe.create({ ...values, authors: authors.value });
+    closeModal();
+  } else {
+    // update author
+    bookStroe.update(book.value._id, { ...values, authors: authors.value });
+    closeModal();
+  }
+});
+
+// Control open/close state of modal
+const open = ref(false);
+const openModal = (item) => {
+  // Set init values if an object is passed
+  if (item)
+    book.value = JSON.parse(
+      JSON.stringify({ ...item, published: item.published.substring(0, 10) })
+    );
+  open.value = true;
+};
+
+const closeModal = () => {
+  book.value = {};
+  authors.value = [];
+  open.value = false;
+};
+
+// Expose these methods to parent components
+defineExpose({
+  openModal,
+  closeModal,
+});
+</script>
---
+++ components/Nav/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="w-full border-b border-gray-200 bg-white/50 backdrop-blur">
+    <div class="flex items-center justify-between h-20 px-3 mx-auto max-w-7xl">
+      <NuxtLink to="/" class="text-xl font-semibold"> CRUD</NuxtLink>
+
+      <nav class="flex items-center space-x-2">
+        <template v-for="n in menu" :key="`menuUtem${n.name}`">
+          <nuxt-link
+            :to="n.link"
+            class="px-3 py-2 text-sm font-medium text-gray-500 transition-all rounded-lg link hover:text-orange-500 hover:bg-orange-500"
+            >{{ n.name }}</nuxt-link
+          >
+        </template>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup>
+const menu = ref([
+  { name: "Books", link: "/" },
+  { name: "Authors", link: "/authors" },
+]);
+</script>
+
+<style scoped>
+.router-link-active.link {
+  @apply text-orange-500 bg-orange-50;
+}
+</style>
